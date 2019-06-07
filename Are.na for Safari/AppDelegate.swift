ee4812f9b70d5da4a2e44b896e528a537a8e2341
Joshua Söhn <joshua.soehn@me.com>
2019-06-07 13:31:29 +0200

    Inital commit

---
+++ Are.na for Safari/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Are.na for Safari
+//
+//  Created by Personal on 07.06.19.
+//  Copyright © 2019 Joshua Söhn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
