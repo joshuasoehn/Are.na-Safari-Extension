17c2e6eddf8085f0ef89895995fa557452973be7
Joshua Söhn <joshua.soehn@me.com>
2019-06-07 15:40:23 +0200

    Added script, added permissions

--- Are.na Extension/SafariExtensionHandler.swift
+++ Are.na Extension/SafariExtensionHandler.swift
@@ -20,6 +20,11 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func toolbarItemClicked(in window: SFSafariWindow) {
         // This method will be called when your toolbar item is clicked.
         NSLog("The extension's toolbar item was clicked")
+        window.getActiveTab(completionHandler: { (activeTab) in
+            activeTab?.getActivePage(completionHandler:  { (activePage) in
+                activePage?.dispatchMessageToScript(withName: "toolbarItemClicked", userInfo: nil)
+            })
+        })
     }
     
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
--- Are.na Extension/script.js
+++ Are.na Extension/script.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-});
+if (window.top === window) {
+    safari.self.addEventListener("message", function (event) {
+        const scr = document.createElement('script')
+        scr.setAttribute('type', 'text/javascript')
+        scr.setAttribute('charset', 'UTF-8')
+        scr.setAttribute('src', 'https://www.are.na/loader.js')
+        document.documentElement.appendChild(scr)
+    });
+}
